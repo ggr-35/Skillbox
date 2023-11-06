6198c4cafde2c8f1ff7b0c95a0a066adc9fa3006
ggr-35 <german.golnikov@yandex.ru>
2023-11-06 19:39:47 +0300

    М26

---
+++ 27/main1.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using std::cout, std::cin, std::endl, std::string, std::map, std::vector;
+
+class TrainCar
+{
+
+private:
+
+    int passengersMax = 0;
+    int passengers = 0;
+
+public:
+
+    void load(int value)
+    {
+        passengers += value;
+
+        if(passengers > passengersMax)
+            passengers = passengersMax;
+    }
+
+    int getPassengers()
+    {
+        return passengers;
+    }
+
+    TrainCar(int inPassengersMax): passengersMax(inPassengersMax)
+    {
+        assert(inPassengersMax >= 0);
+    }
+
+};
+
+class Train
+{
+private:
+    int count = 0;
+    TrainCar** cars = nullptr;
+
+public:
+
+    TrainCar* gatCarAt(int index)
+    {
+        if (index < 0) return nullptr;
+        if (index >= count) return nullptr;
+
+        return cars[index];
+    }
+
+    int getCount()
+    {
+        return count;
+    }
+
+    Train(int inCount, int inPassengersMax): count(inCount)
+    {
+        cars = new TrainCar*[inCount]; // Создается динамический массив указателей на вагоны, называемый "cars", длиной inCount.
+        for (int i = 0; i < count; i++)
+        {
+            cars[i] = new TrainCar(inPassengersMax); // создается новый объект TrainCar со значением inPassengersMax, и указатель на этот объект присваивается текущему элементу массива cars.
+        }
+        
+    }
+
+};
+
+
+int main()
+{
+   Train* train = new Train(10, 30);
+
+   for (int i = 0; i < train->getCount(); i++)
+   {
+        int passengers = 0;
+        cin >> passengers;
+        train->gatCarAt(i)->load(passengers);
+   }
+   
+}
